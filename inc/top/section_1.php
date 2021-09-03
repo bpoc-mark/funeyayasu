d70666bcd482bc7799fa04d431421371a198d3b7
denver <denver.gomez@bpoc.co.jp>
2021-09-03 09:16:40 +0800

    update contents

--- inc/top/section_1.php
+++ inc/top/section_1.php
@@ -6,11 +6,11 @@
             <img class="sect_1__img-map" src="<?php echo get_template_directory_uri(); ?>/release/image/sect_1/map.png"
                 alt="">
             <div class="l-content sect_1__description">
-                <p class="c-heading--01 c-heading--white"> JR草津駅から徒歩4分、<br class="br-pc"> <br class="br-sp">
-                    魚介は産地直送や、<br class="br-sp">毎日市場に買付け。<br class="br-pc"> <br class="br-sp">
-                    新鮮魚介居酒屋。<br class="br-pc"> <br class="br-sp">
-                    47都道府県の日本酒 <br class="br-sp">
-                    取り揃えています。
+                <p class="c-heading--01 c-heading--white"> ＪＲ野洲駅徒歩1分。<br class="br-pc"> <br class="br-sp">
+                    毎日買い付けをし、<br class="br-sp"><br class="br-pc">
+                    産地直送で仕入れる新鮮魚介！<br class="br-pc"> <br class="br-sp">
+                    北海道から沖縄まで47種類の<br class="br-sp"><br class="br-pc">
+                    日本酒を取り揃えています。
                 </p>
             </div>
             <div class="l-container sect_1__center-col-1">
@@ -31,15 +31,15 @@
 
                 <?php if ($the_query->have_posts()) : ?>
 
-                    <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+                <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
 
-                        <div class="sect_1__center--frame">
-                            <?php the_post_thumbnail(); ?>
-                            <div class="c-heading-title c-heading-title-white mt-20"><?php the_title(); ?></div>
-                            <div class="text text-white"><?php the_content(); ?></div>
-                        </div>
+                <div class="sect_1__center--frame">
+                    <?php the_post_thumbnail(); ?>
+                    <div class="c-heading-title c-heading-title-white mt-20"><?php the_title(); ?></div>
+                    <div class="text text-white"><?php the_content(); ?></div>
+                </div>
 
-                    <?php endwhile; ?>
+                <?php endwhile; ?>
                 <?php endif; ?>
             </div>
         </div>
--- inc/top/section_3.php
+++ inc/top/section_3.php
@@ -22,14 +22,11 @@
                                 src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/star.png" alt="">
                             <img class="star-pic"
                                 src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/star.png" alt="">
-                            <span>3 週間前 <small>新規</small></span>
+                            <span>1 年前</span>
                         </div>
-                        <p class="c-rating">美味しい料理とお酒<img
-                                src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/liquior_icon.png"
-                                alt=""><br>
-                            楽しい時間で時がたつのをすぐ忘れてしまいます<img
-                                src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/smiley_icon.png"
-                                alt=""></p>
+                        <p class="c-rating">コスパ最高です。<br>
+                            また、利用したい
+                        </p>
                     </div>
                     <!-- Row Star 2 -->
                     <div class="border-b c-pad">
@@ -44,12 +41,14 @@
                                 src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/star.png" alt="">
                             <img class="star-pic"
                                 src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/star.png" alt="">
-                            <span>6か月前</span>
+                            <span>4 年前</span>
                         </div>
                         <p class="c-rating">
-                            雰囲気の良いお店ですお料理も一つ一つか凝っていて目でも楽しめるモノばかりです♪お野菜もお魚も新鮮で本当に美味しいお料理ばかり、フネヤパフェも是非食べてもらいたいもらいたいお勧めデザートです<br>
-                            <img src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/heart_icon_w.png"
-                                alt="">
+                            最高の食・酒。<br>
+                            最高の空間で一時を楽しめました。<br>
+                            <br>
+                            感謝。
+
                         </p>
                         <!-- <div class="inline-img">
                             <div> <img class="food-1"
@@ -79,20 +78,12 @@
                                 src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/star.png" alt="">
                             <img class="star-pic"
                                 src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/star.png" alt="">
-                            <span>4 か月前</span>
+                            <span>2 か月前</span>
                         </div>
-                        <p class="c-rating">連休中だったのですが運良く入れました<img
-                                src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/smiley_close_eye_icon.png"
-                                alt=""><br>
-                            お店の方達がとっても感じが良くて、旅の楽しさがましました<img
-                                src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/heart_icon_r.png"
-                                alt=""><br>
-                            <br>
-                            土鍋の鯛めしが絶品<span>!</span>最後に出汁をかけて食べる食べ方は初めてでした。広島の牡蠣も生臭くなく一人前の料理も多い、そして契約農家さんが作っている野菜が美味しかった。<br>
-                            <br>
-                            料理も工夫があり、感じもいいし、良い店見つけました<img
-                                src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/smiley_close_eye_icon.png"
-                                alt="">
+                        <p class="c-rating">初めて野洲駅下車。お通しがいきなり美味しくて驚き。鯖が好きなのでサバ飯的なものを注文。時間がかかりそうなので刺し身の盛り合わせも。<br>
+                            程よく刺し身を食べ終わったあたりに釜飯（山盛）が届き、一人では多かったかな？と思いながら完食しました。<br>鯖脂と出汁を交互に合わせるといつまでも食べれる。<br>
+                            美味しいね。何食べても美味しそうでした。次はコースにチャレンジしたい。
+
                         </p>
                         <div class="inline-img">
                             <div> <img class="food-1"
@@ -130,14 +121,9 @@
                                 src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/star.png" alt="">
                             <img class="star-pic"
                                 src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/star.png" alt="">
-                            <span>2 年前</span>
+                            <span>9 か月前</span>
                         </div>
-                        <p class="c-rating">前にあった守山の頃から通って10年位になります。<br>
-                            海のない滋賀県でもこれだけ新鮮な海の幸が食べれるのはここくらいじゃないでしょうか。<br>
-                            経営者のマスターを筆頭にアルバイトも美男美女が揃っています。<br>
-                            安くて美味しく楽しいお店です。<br>
-                            これ以上混み合って欲しくないので本当は教えたくないんですがww
-                            予約する事をお勧めします。</p>
+                        <p class="c-rating">変わったメニューもあって、美味しくてよかったです。</p>
                         <div class="inline-img">
                             <div> <img class="food-1"
                                     src="<?php echo get_template_directory_uri(); ?>/release/image/sect_3/img_10.png"

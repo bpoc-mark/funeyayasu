9725bd1d69c6d99a2122fb992e45ec67bc76c458
arsua-greg <gregarsua@bpoc.co.jp>
2021-09-30 10:23:59 +0800

    adjust image pixelated

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
@@ -11,19 +11,15 @@
     <?php wp_head(); ?>
     <?php wp_footer(); ?>
 
-    <link rel="shortcut icon" type="image/jpg"
-        href="<?php echo get_template_directory_uri(); ?>/release/image/funuyakusatsu-fav.png" />
+    <link rel="shortcut icon" type="image/jpg" href="<?php echo get_template_directory_uri(); ?>/release/image/funuyakusatsu-fav.png" />
 
     <!-- CSS -->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/release/css/style.css">
     <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700;900&family=Noto+Serif+JP:wght@400;700;900&display=swap"
-        rel="stylesheet">
-    <link rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/cross-platform-yu-gothic@0.1.1/cross-platform-yu-gothic.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700;900&family=Noto+Serif+JP:wght@400;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/cross-platform-yu-gothic@0.1.1/cross-platform-yu-gothic.min.css">
     <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css" integrity="sha512-17EgCFERpgZKcm0j0fEq1YCJuyAWdz9KUtv1EjVuaOz8pDnh/0nZxmU6BBXwaaxqoi9PQXnRWqlcDB027hgv9A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css" integrity="sha512-yHknP1/AwR+yx26cB1y0cjvQUMvEa2PFzt1c9LlS4pRQ5NOTZFWbhBig+X9G9eYW/8m0/4OXNx8pxJ6z57x0dw==" crossorigin="anonymous" referrerpolicy="no-referrer" /> -->
 
@@ -42,14 +38,12 @@
                 $imgurl = wp_get_attachment_url(get_post_thumbnail_id($title));
             }
             ?>
-            <div class="page__top isHome"
-                style="background: url('<?php echo $imgurl; ?>') !important; background-size: cover !important;background-position: center center;background-repeat: no-repeat;">
+            <div class="page__top isHome" style="background: url('<?php echo $imgurl; ?>') !important; background-size: cover !important;background-position: center center;background-repeat: no-repeat;">
                 <div class="page__wrapper">
                     <div class="l-wrap">
                         <div class="page__info">
                             <div class="info_logo">
-                                <img src="<?php echo get_template_directory_uri(); ?>/release/image/header/info_logo.png"
-                                    alt="">
+                                <img src="<?php echo get_template_directory_uri(); ?>/release/image/header/info_logo.png" alt="">
                             </div>
                             <div class="info_txt">
                                 <h2>魚介と言えば、<br>FUNEYAです。</h2>
@@ -58,11 +52,9 @@
                     </div>
                     <div class="hero_cta">
                         <div class="hero_cta__cont" id="hero_cta">
-                            <a class="item_01" href="https://www.tablecheck.com/shops/funeya-yasu/reserve"
-                                target="_blank">
+                            <a class="item_01" href="https://www.tablecheck.com/shops/funeya-yasu/reserve" target="_blank">
                                 <div class="item_icn">
-                                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/header/icon_note.png"
-                                        alt="">
+                                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/header/icon_note.png" alt="">
                                 </div>
                                 <div class="item_txt">
                                     <p>
@@ -73,8 +65,7 @@
                             </a>
                             <a class="item_01" href="https://funeya-yasu.owst.jp/" target="_blank">
                                 <div class="item_icn">
-                                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/header/icon_box.png"
-                                        alt="">
+                                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/header/icon_box.png" alt="">
                                 </div>
                                 <div class="item_txt">
                                     <p>
@@ -84,8 +75,7 @@
                             </a>
                             <a class="item_01" href="https://line.me/R/ti/p/%40621hekkl" target="_blank">
                                 <div class="item_icn">
-                                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/header/icon_line.png"
-                                        alt="">
+                                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/header/icon_line.png" alt="">
                                 </div>
                                 <div class="item_txt">
                                     <p>
@@ -102,10 +92,8 @@
                     <div class="p-header__left">
                         <a href="">
                             <h1>
-                                <img src="<?php echo get_template_directory_uri(); ?>/release/image/header/header_logo_pc.png"
-                                    alt="">
-                                <img src="<?php echo get_template_directory_uri(); ?>/release/image/header/header_logo_sp.png"
-                                    alt="">
+                                <img src="<?php echo get_template_directory_uri(); ?>/release/image/header/header_logo_pc.png" alt="">
+                                <img src="<?php echo get_template_directory_uri(); ?>/release/image/header/header_logo_sp.png" alt="">
                             </h1>
                         </a>
                     </div>
